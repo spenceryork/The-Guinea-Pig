7cce2603bc033809551e0bd12069debcc49ba51b
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2017-12-03 19:23:51 -0600

    finished the exercise

--- events.js
+++ events.js
@@ -17,10 +17,46 @@ let h1 = document.getElementById("page-title");
 h1.addEventListener("mouseover", mouseOver);
 function mouseOver() {
     output.innerHTML = "You moved your mouse over the header";
-}
+};
 
 // When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
 h1.addEventListener("mouseout", mouseOff);
 function mouseOff () {
     output.innerHTML = "You left me!!";
+};
+
+// When you type characters into the input field, the output element should mirror the text in the input field.
+let keyInpt = document.getElementById("keypress-input");
+keyInpt.addEventListener("keyup", keyP);
+function keyP() {
+    output.innerHTML = keyInpt.value;
+};
+
+// When you click the "Add color" button, the guinea-pig element's text color should change to blue.
+let addCbtn = document.getElementById("add-color");
+let guinea = document.getElementById("guinea-pig");
+addCbtn.addEventListener("click", newColor);
+function newColor() {
+    guinea.classList.toggle("colorBlue");
+};
+
+// When you click the "Hulkify" button, the guinea-pig element's font size should become much larger.
+let hulkBtn = document.getElementById("make-large");
+hulkBtn.addEventListener("click", hulk);
+function hulk() {
+    guinea.classList.toggle("big");
+}
+
+// When you click the "Capture it" button, the guinea-pig element should have a border added to it.
+let capture = document.getElementById("add-border");
+capture.addEventListener("click", captureFunc);
+function captureFunc() {
+    guinea.classList.toggle("cage");
+}
+
+// When you click the "Rounded" button, the guinea-pig element's border should become rounded.
+let rounded = document.getElementById("add-rounding");
+rounded.addEventListener("click", roundMe);
+function roundMe() {
+    guinea.classList.toggle("round");
 }
